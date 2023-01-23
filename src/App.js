a90a4958e76dedd5f05320b36232ec09b2c9b02a
NAMRATA <namratajhait@gmail.com>
2023-01-23 16:02:56 +0530

    changes|

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+import Practice from './Component/Practice';
+
+function App() {
+  return (
+    <div className="App">
+        
+      <Practice />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Practice/index.js
@@ -0,0 +1,129 @@
+export default function Practice(){
+//     let arr = [45,23,21]
+//     // Array map method
+//     let a =arr.map((value, index, array)=>{
+//         console.log(value, index, array)
+//         return value + index
+//     })
+//     console.log(a)
+
+
+// Array filter method
+// let arr2 =[45, 23, 21, 0, 3, 5]
+// let a2 = arr2.filter((a)=>{
+//     return a < 10
+// })
+// console.log(a2)
+
+
+// let arr3 =[1, 2, 3, 5, 2, 1]
+// let newarr3 = arr3.reduce((h1, h2)=>{
+//     return h1+h2
+// })
+// console.log(newarr3)
+
+// Array method
+// let num = [1, 2, 3, 34, 4]
+// let b = num.toString()//b is now a string
+ //console.log(b, typeof b)
+ 
+ // join method
+//let num = [1, 2, 45, 46]
+//let c =num.join(" and ")
+//console.log(c, typeof c)
+ // pop method
+//  let r = num.pop() //pop returns a poped element
+//  console.log(num, r)
+// let r = num.push(56) //push return a new array length 
+// console.log(num, r)
+
+// let r = num.shift() // remove an element from the start of an array
+// console.log(r, num)
+
+// console.log(num.length)
+// delete num[0]
+// console.log(num.length)
+
+// sort method
+// let num = [551, 34, 58, 88]
+// num.sort()
+// console.log(num)
+
+// concat method
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// let num_more = [11, 12, 13, 14, 15, 16, 17, 18, 19]
+// let num_even_more =[211, 212, 213, 214, 415, 416, 417, 418, 419]
+// let newArray = num.concat(num_more, num_even_more)
+// console.log(newArray)
+// console.log(num, num_more)
+ 
+// sort method
+// let compare = (a, b)=>{
+//     return b - a
+// }
+//  let num = [551, 34, 58, 88]
+//  num.sort(compare)
+//  console.log(num)
+
+// using map method convert 
+// function convertMiToKm(arr){
+//     return arr.map (mile => mile*0.621);
+// }
+// function totalDis ( acc, cur){
+//     return acc + cur;
+// }
+// const km =convertMiToKm ([34, 2, 3, 4]);
+// console.log ( km )
+// let totalDisInKm = km.reduce (totalDis, 0);
+// console.log (totalDisInKm);
+
+// let dailyActivities = ['sleep', 'work', 'study']
+// let newRountine = ['eat']
+// let newWork = ['Excersice', 'hardwork']
+// dailyActivities.sort(); //sorting element in alphabetical order
+// console.log(dailyActivities);
+// // concatenating two arrays
+// let rountine = dailyActivities.concat(newRountine);
+// console.log(rountine);
+// // finding the index position of string
+// let position = dailyActivities.indexOf('sleep')
+// console.log(position)
+// slicing the array elements
+// let newDailyAct = dailyActivities.slice(1);
+// console.log(newDailyAct);
+
+// splice method
+// let months =['jan', 'feb', 'april', 'june'];
+// months.splice(2, 0, 'march');
+// console.log(months)
+// months.splice(4, 0, 'may');
+// console.log(months)
+// months.splice(6, 1, 'july');
+// console.log(months)
+// // shuft method
+// let month = months.shift();
+// console.log(month)
+// let array = Array.of('june')
+// console.log(array)
+
+// let array1 = [4, 6, 7, 8]
+// let a = array1.map(x=>x*2)
+// console.log(a)
+
+// console.log(array1.pop())
+// let b = array1.push(3);
+// console.log(b)
+// console.log(array1)
+
+// console.log(array1.unshift(3, 2))
+// console.log(array1)
+
+
+
+
+
+
+
+
+
+}
